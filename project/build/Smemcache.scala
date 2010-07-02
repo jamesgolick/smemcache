6aa7e99683f49d0b05d90964e7d0bb1c447f9734
James Golick <james@giraffesoft.ca>
2010-07-02 15:13:54 -0400

    RC3, etc

--- project/build/Smemcache.scala
+++ project/build/Smemcache.scala
@@ -1,9 +1,11 @@
 import sbt._
 
 class SmemcacheProject(info: ProjectInfo) extends DefaultProject(info) {
-    val specs        = "org.specs" % "specs" % "1.6.0" from "http://specs.googlecode.com/files/specs-1.6.0.jar"
-    val mockito      = "org.mockito" % "mockito" % "1.8.0" from "http://mockito.googlecode.com/files/mockito-all-1.8.0.jar"
-    val spymemcached = "net.spy.memcached" % "spymemcached" % "2.4.2" from "http://spymemcached.googlecode.com/files/memcached-2.4.2.jar"
+  val codaRepo      = "Coda Hale's Repository" at "http://repo.codahale.com/"
+  val specsRepo     = "Specs Repo" at "http://nexus.scala-tools.org/content/repositories/snapshots"
+  val specs         = "org.scala-tools.testing" % "specs_2.8.0.RC3" % "1.6.5-SNAPSHOT"
+  val mockito       = "org.mockito" % "mockito" % "1.8.4" from "http://mockito.googlecode.com/files/mockito-all-1.8.4.jar"
+  val spymemcached  = "net.spy.memcached" % "spymemcached" % "2.4.2" from "http://spymemcached.googlecode.com/files/memcached-2.4.2.jar"
+  val snapshotsRepo = "Scala Tools Snapshots Repository" at "http://scala-tools.org/repo-snapshots/"
+  val logula        = "com.codahale" %% "logula" % "1.0.1" withSources()
 }
-
-// vim: set ts=4 sw=4 et:
