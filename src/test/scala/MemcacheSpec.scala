18f6d027c7b54db0cede43151a65f8ed896fd19e
James Golick <james@giraffesoft.ca>
2010-07-02 20:31:45 -0400

    spacing

--- src/test/scala/MemcacheSpec.scala
+++ src/test/scala/MemcacheSpec.scala
@@ -15,59 +15,59 @@ import org.specs.mock.Mockito
 import org.mockito.Matchers._
 
 object MemcacheSpec extends Specification with Mockito {
-    implicit def convertScalaListToJavaList(aList:List[String]) =
-        java.util.Arrays.asList(aList.toArray: _*)
+  implicit def convertScalaListToJavaList(aList:List[String]) =
+      java.util.Arrays.asList(aList.toArray: _*)
 
-    "implicitly converting a string to InetSocketAddress" should {
-        "split the host and port" in {
-            val addr: InetSocketAddress = "localhost:1234"
-            addr.getHostName must_== "localhost"
-            addr.getPort must_== 1234
-        }
-
-        "set the port to 11211 if its not in the string" in {
-            val addr: InetSocketAddress = "localhost"
-            addr.getHostName must_== "localhost"
-            addr.getPort must_== 11211
-        }
+  "implicitly converting a string to InetSocketAddress" should {
+    "split the host and port" in {
+      val addr: InetSocketAddress = "localhost:1234"
+      addr.getHostName must_== "localhost"
+      addr.getPort must_== 1234
     }
 
-    val underlyingClient = mock[MemcachedClient]
-    val cache            = new Memcache(underlyingClient)
-
-    "doing a cache get delegates to the underlying client" in {
-      underlyingClient.get("someKey") returns "some value"
-      cache.get[String]("someKey") must_== "some value"
-      there was one(underlyingClient).get("someKey")
+    "set the port to 11211 if its not in the string" in {
+      val addr: InetSocketAddress = "localhost"
+      addr.getHostName must_== "localhost"
+      addr.getPort must_== 11211
     }
+  }
 
-    val callable = new Callable[JBool]() {
-        def call() = new JBool(true)
+  val underlyingClient = mock[MemcachedClient]
+  val cache            = new Memcache(underlyingClient)
+
+  "doing a cache get delegates to the underlying client" in {
+    underlyingClient.get("someKey") returns "some value"
+    cache.get[String]("someKey") must_== "some value"
+    there was one(underlyingClient).get("someKey")
+  }
+
+  val callable = new Callable[JBool]() {
+      def call() = new JBool(true)
+  }
+  val future = new FutureTask[java.lang.Boolean](callable)
+
+  "doing a set sets a default expiry in the client" in {
+    underlyingClient.set("someKey", 0, "someValue") returns future
+    cache.set("someKey", "someValue")
+    got {
+      underlyingClient.set("someKey", 0, "someValue")
     }
-    val future = new FutureTask[java.lang.Boolean](callable)
+  }
 
-    "doing a set sets a default expiry in the client" in {
-      underlyingClient.set("someKey", 0, "someValue") returns future
-      cache.set("someKey", "someValue")
-      got {
-        underlyingClient.set("someKey", 0, "someValue")
-      }
+  "doing a set has a signature that accepts an expiration" in {
+    underlyingClient.set("someKey", 9, "someValue") returns future
+    cache.set("someKey", "someValue", 9)
+    got {
+      underlyingClient.set("someKey", 9, "someValue")
     }
+  }
 
-    "doing a set has a signature that accepts an expiration" in {
-      underlyingClient.set("someKey", 9, "someValue") returns future
-      cache.set("someKey", "someValue", 9)
-      got {
-        underlyingClient.set("someKey", 9, "someValue")
-      }
-    }
+  "doing a multiGet delegates to the client" in {
+    val map      = new java.util.HashMap[String, Object]()
+    val scalaMap = Map("key1" -> "value1")
 
-    "doing a multiGet delegates to the client" in {
-      val map      = new java.util.HashMap[String, Object]()
-      val scalaMap = Map("key1" -> "value1")
-
-      map.put("key1", "value1")
-      underlyingClient.getBulk(List("key1", "key2")) returns map
-      cache.multiGet(List("key1", "key2")) must_== scalaMap
-    }
+    map.put("key1", "value1")
+    underlyingClient.getBulk(List("key1", "key2")) returns map
+    cache.multiGet(List("key1", "key2")) must_== scalaMap
+  }
 }
